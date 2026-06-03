61d6fcd2825c37930aa765fca3b30db1ddc22377
Jagadish Pradhan <165784645+Rohan7624@users.noreply.github.com>
2026-06-03 11:38:20 +0530

    Initial commit - inventory reservation system

---
+++ src/main/java/com/allo/InventoryReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.allo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryReservationSystemApplication.class, args);
+	}
+
+}
